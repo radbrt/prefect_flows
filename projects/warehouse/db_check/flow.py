c676015b246192ee92a75229ba7b4071bd8ace86
Henning <henning.holgersen@gmail.com>
2022-04-05 20:28:26 +0200

    added artifact flow

---
+++ projects/warehouse/db_check/flow.py
@@ -0,0 +1,34 @@
+from prefect import task, Flow
+from prefect.executors import LocalDaskExecutor
+from prefect.run_configs import KubernetesRun
+from prefect.tasks.secrets import PrefectSecret
+from prefect.storage import Docker
+from prefect.artifacts import create_markdown
+from prefect.backend.artifacts import create_markdown_artifact
+import prefect
+
+import random
+
+import datetime
+
+@task
+def check_db_quality():
+    create_markdown_artifact(
+        f"""
+        # Sample Artifact
+        The randomly generated value is {random.random()}
+
+        Generated at {datetime.datetime.utcnow().isoformat()}
+        """)
+
+dockerstore = Docker(
+    image_name='db_check',
+    registry_url='radbrt.azurecr.io',
+    dockerfile='Dockerfile'
+)
+
+with Flow("DWH Quality check", storage=dockerstore) as flow:
+    check_db_quality()
+
+flow.run_config = KubernetesRun(labels=["aks"])
+flow.executor = LocalDaskExecutor()
